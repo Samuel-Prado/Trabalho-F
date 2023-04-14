c0bb3b5df1206f04028b7407f9e3116b6e2f990b
Samuel-Prado <117532897+Samuel-Prado@users.noreply.github.com>
2023-04-14 00:04:41 -0300

    Aa

--- assets/js/js.js
+++ assets/js/js.js
@@ -3,10 +3,5 @@ var btnMob = document.querySelector(".btnMub");
 
 btnMob.addEventListener('click', function() {
 
-    var btnMob1 = document.querySelector(".linha1");
-    var btnMob2 = document.querySelector(".linha2");
-    var btnMob3 = document.querySelector(".linha3");
-    btnMob1.classList.toggle("ativo");
-    btnMob2.classList.toggle("ativo");
-    btnMob3.classList.toggle("ativo");
+    Alert(",ou");
 });
