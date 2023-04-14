c149a0da2014f19bff10eb4ad2e94b9fd4f17f55
Samuel-Prado <samuel.prado@estudante.iftm.edu.br>
2023-04-13 22:51:12 -0300

    up

---
+++ assets/js/js.js
@@ -0,0 +1,12 @@
+
+var btnMob = document.querySelector(".btnMub");
+
+document.addEventListener('click', function() {
+
+    var btnMob1 = document.querySelector(".linha1");
+    var btnMob2 = document.querySelector(".linha2");
+    var btnMob3 = document.querySelector(".linha3");
+    btnMob1.classList.toggle("ativo");
+    btnMob2.classList.toggle("ativo");
+    btnMob3.classList.toggle("ativo");
+});
